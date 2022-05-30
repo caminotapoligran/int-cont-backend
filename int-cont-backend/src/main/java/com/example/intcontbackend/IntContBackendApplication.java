ed601cba183bd6b67c01f23d6fa6fba5d6bc0f99
Marlon Rodriguez <cerokool32@gmail.com>
2022-05-30 14:16:48 -0500

    base de codigo

---
+++ int-cont-backend/src/main/java/com/example/intcontbackend/IntContBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.intcontbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntContBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntContBackendApplication.class, args);
+    }
+
+}
---
+++ int-cont-backend/src/test/java/com/example/intcontbackend/IntContBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intcontbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntContBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
